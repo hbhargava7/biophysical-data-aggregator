b4da51e86b4667f06cd0d56d5bfec57675b571bd
AdamoMancino <adamo.mancino@mail.mcgill.ca>
2019-09-26 14:40:25 -0700

    Updated to work on Python3

--- PDB.py
+++ PDB.py
@@ -1,34 +1,34 @@
-import urllib2
+import urllib.request as urllib
 
 
-url = 'http://www.rcsb.org/pdb/rest/search'
+def getPDBIDs(input):
+    url = 'http://www.rcsb.org/pdb/rest/search'
 
-search = "Nav1.1"
+    input = "Nav1.1"
 
-queryText = """<?xml version="1.0" encoding="UTF-8"?><orgPdbQuery><queryType>org.pdb.query.simple.MoleculeNameQuery</queryType><description>Molecule Name Search : Molecule Name=""" + search + """</description><macromoleculeName>""" + search + """</macromoleculeName></orgPdbQuery>"""
+    queryText = """<?xml version="1.0" encoding="UTF-8"?><orgPdbQuery><queryType>org.pdb.query.simple.MoleculeNameQuery</queryType><description>Molecule Name Search : Molecule Name=""" + input + """</description><macromoleculeName>""" + input + """</macromoleculeName></orgPdbQuery>"""
+    queryBytes = str.encode(queryText)
+
+    req = urllib.Request(url, data=queryBytes)
+
+    f = urllib.urlopen(req)
+    resultBytes = f.read()
+    resultsText = str(resultBytes, 'utf-8')
+    print(type(resultsText))
+
+    resultList = resultsText.split("\n")
+    if (resultList[-1] == ""):
+        del(resultList[-1])
+    updatedPDBIDs = []
+    for name in resultList:
+        updatedPDBIDs.append(name[0:4])
+
+    return updatedPDBIDs
 
 
-print "query:\n", queryText
+if __name__ == "__main__":
+    searchTerm = "Hsp90"
+    listPDBs = getPDBIDs(searchTerm)
+    print(listPDBs)
 
-print "querying PDB...\n"
-
-req = urllib2.Request(url, data=queryText)
-
-f = urllib2.urlopen(req)
-
-result = f.read()
-print(result)
-result = result.split("\n")
-#del(result[-1])
-print(len(result))
-for name in result:
-    print(name[0:4])
-quit()
-
-if result:
-
-    print "Found number of PDB entries:", result.count('\n')
-
-else:
-
-    print "Failed to retrieve results"
+    
